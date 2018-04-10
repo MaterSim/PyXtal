62e2594aec16a916701066b0459fa46556c2dd76
Qiang Zhu <alecfans@gmail.com>
2018-04-10 15:01:27 -0700

    This is a file for running the test

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from structure import *
+from structure import random_crystal
+from vasp import read_vasp
+import spglib
 
-factor = 2.0
 #Test paramaters: [sg_min, sg_max], [species], [numIons]
 params_list = [
 [[2,230],[3],[8]],
@@ -11,16 +12,17 @@ params_list = [
 [[2,15],[3],[4]],
 [[143,167],[3],[4]]
 ]
+
+factor = 1.0
 for params in params_list:
     sg_min, sg_max = params[0][0], params[0][1]
     species, numIons = params[1], params[2]
     for sg in range(sg_min, sg_max+1):
         rand_crystal = random_crystal(sg, species, numIons, factor)
-        new_struct, good_struc = rand_crystal.struct, rand_crystal.good_struct
-        if good_struc:
-            new_struct.to(fmt="poscar", filename = '1.vasp')
+
+        if rand_crystal.good_struct:
+            rand_crystal.struct.to(fmt="poscar", filename = '1.vasp')
             cell = read_vasp('1.vasp')
-            #ans = spglib.get_spacegroup(cell)
             ans = spglib.get_symmetry_dataset(cell, symprec=1e-1)['number']
             print('Space group  requested: ', sg, 'generated', ans)
             if ans < int(sg/1.2):
@@ -33,5 +35,5 @@ for params in params_list:
         else: 
             print('something is wrong')
             #print(len(new_struct.frac_coords))
-            break
             #print(new_struct)
+            break
