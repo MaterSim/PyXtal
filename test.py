c56f53682f6bfa5db2d713f0ef12cb4ee845a025
Scott <swf767@gmail.com>
2018-04-19 22:41:57 -0700

    Add text flags for outputs with lower-symmetry spacegroups

--- test.py
+++ test.py
@@ -19,6 +19,7 @@ factor = 1.0
 for params in params_list:
     sg_min, sg_max = params[0][0], params[0][1]
     species, numIons = params[1], params[2]
+    print("-----------------Species:"+str(species)+", NumIons:"+str(numIons)+"----------------------")
     for sg in range(sg_min, sg_max+1):
         #profile.run("random_crystal(sg, species, numIons, factor)")
         rand_crystal = random_crystal(sg, species, numIons, factor)
@@ -26,7 +27,12 @@ for params in params_list:
         if rand_crystal.valid:
             rand_crystal.struct.to(fmt="poscar", filename = '1.vasp')
             ans = get_symmetry_dataset(rand_crystal.spg_struct, symprec=1e-1)['number']
-            print('Space group  requested: ', sg, 'generated', ans)
+            #Flag outputs with (potentially) lower-symmetry spacegroups
+            flag = ""
+            if ans<sg:
+                flag = "****"
+                if ans<sg/1.2: flag = "****XXXX"
+            print('Space group  requested: ', sg, 'generated', ans, flag)
             #print(CifWriter(rand_crystal.struc, symprec=0.1).__str__())
             #print('Space group:', finder.get_space_group_symbol(), 'tolerance:', tol)
             #output wyckoff sites only
