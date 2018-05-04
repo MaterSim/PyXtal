ccb4b2d18b6b8fa789cde5101c3f0ed80a7c6a11
Scott <swf767@gmail.com>
2018-05-03 17:39:07 -0700

    Convert get_inertia_tensor to pmg-type

--- molecule.py
+++ molecule.py
@@ -5,11 +5,13 @@ from ase.build import molecule
 import numpy as np
 
 def get_inertia_tensor(mol):
-    #Calculate the inertia tensor for an ASE molecule
-    com = mol.get_center_of_mass()
-    positions = mol.get_positions()
-    positions -= com  # translate center of mass to origin
-    masses = mol.get_masses()
+    #Calculate the inertia tensor for a PMG Molecule object
+    positions = []
+    masses = []
+    for x in mol:
+        positions.append(x.coords)
+        masses.append(x.specie.number)
+    positions -= mol.center_of_mass  # translate center of mass to origin
 
     # Initialize elements of the inertial tensor
     I11 = I22 = I33 = I12 = I13 = I23 = 0.0
@@ -29,20 +31,25 @@ def get_inertia_tensor(mol):
                   [I13, I23, I33]])
 #Test Functionality
 #---------------------------------------------------
-from structure import *
 #Test cases: water, methane, and c60 via pymatgen
-'''h20 = Molecule.from_file('xyz/water.xyz')
+h20 = Molecule.from_file('xyz/water.xyz')
 ch4 = Molecule.from_file('xyz/methane.xyz')
 c60 = Molecule.from_file('xyz/c60.xyz')
 pga_h20 = PointGroupAnalyzer(h20)
 pga_ch4 = PointGroupAnalyzer(ch4)
 pga_c60 = PointGroupAnalyzer(c60)
+
+#Symmetrize ch4
+ch4 = pga_ch4.symmetrize_molecule()['sym_mol']
+pga_ch4 = PointGroupAnalyzer(ch4)
+
 pg_h20 = pga_h20.get_pointgroup()
 pg_ch4 = pga_ch4.get_pointgroup()
-pg_c60 = pga_c60.get_pointgroup()'''
+pg_c60 = pga_c60.get_pointgroup()
+
 
 #Test cases: water and methane via ASE
-h2 = molecule('H2')
+'''h2 = molecule('H2')
 water = molecule('H2O')
 methane = molecule('CH4')
 c60 = molecule('C60')
@@ -52,5 +59,6 @@ newpos = [
 [ 0.2051,  0.824,  -0.6786], #H
 [ 0.3345, -0.9314, -0.4496], #H
 [-1.0685, -0.0537,  0.1921]] #H
-methane.set_positions(newpos, apply_constraint=False)
-print(get_inertia_tensor(methane))
+methane.set_positions(newpos, apply_constraint=False)'''
+
+print(get_inertia_tensor(ch4))
