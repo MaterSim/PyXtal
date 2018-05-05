5ee40a9c0fafd7e676032b42796d3128c0e3dc6e
Scott <swf767@gmail.com>
2018-05-04 18:30:58 -0700

    Move test functionality to inside (if __name__ == "__main__")

--- matrix.py
+++ matrix.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 import random
-import pymatgen
+from pymatgen.core.operations import SymmOp
 
 #returns a 3x3 rotation matrix with random angle and orientation
 def aa2matrix(axis='random',angle='random', radians=True):
@@ -17,12 +17,16 @@ def aa2matrix(axis='random',angle='random', radians=True):
         x = math.cos(phi) * math.sin(theta)
         y = math.sin(phi) * math.sin(theta)
         z = math.cos(theta)
+    else:
+        x = axis[0]
+        y = axis[1]
+        z = axis[2]
     if angle == 'random':
     	angle = random.random() * 2 * math.pi
     else:
         if radians is False:
             angle *= 180./math.pi
-    return pymatgen.core.operations.SymmOp.from_origin_axis_angle([0,0,0], [x,y,z], angle, angle_in_radians=True).rotation_matrix
+    return SymmOp.from_origin_axis_angle([0,0,0], [x,y,z], angle, angle_in_radians=True).rotation_matrix
 
 def matrix2aa(m):
     '''
@@ -36,13 +40,14 @@ def matrix2aa(m):
     return [x,y,z], angle
 
 #Test Functionality
+if __name__ == "__main__":
 #----------------------------------------------------
-'''from structure import *
-for x in range(100):
-    mat1 = random_matrix()
-    paras1 = matrix2para(mat1)
-    mat2 = para2matrix(paras1, format='upper')
-    paras2 = matrix2para(mat2)
-    if not np.allclose(paras1, paras2):
-        print(paras1)
-        print(paras2)'''
+    m1 = aa2matrix()
+    aa1 = matrix2aa(m1)
+    print(aa1)
+    m2 = aa2matrix(axis=aa1[0], angle=aa1[1])
+    aa2 = matrix2aa(m2)
+    print(aa2)
+    m3 = aa2matrix(axis=aa2[0], angle=aa2[1])
+    aa3 = matrix2aa(m3)
+    print(aa3)
