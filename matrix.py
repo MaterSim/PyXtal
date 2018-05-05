eb9d255045a4a13de75d5964036a61ba1f964b6f
Scott <swf767@gmail.com>
2018-05-04 18:05:15 -0700

    Update comments and basic functionality

--- matrix.py
+++ matrix.py
@@ -4,8 +4,12 @@ import random
 import pymatgen
 
 #returns a 3x3 rotation matrix with random angle and orientation
-def aa2matrix(axis='random',angle='random'):
-    #If axis is provided, it is assumed to be unit
+def aa2matrix(axis='random',angle='random', radians=True):
+    '''
+    Return a rotation matrix from an axis and angle
+    If axis and/or angle is not provided, a random value is chosen
+    '''
+    #If axis is provided, it is assumed to be unit length
     if axis == 'random':
         phi = random.random() * 2. * math.pi
         N = random.random()
@@ -15,20 +19,21 @@ def aa2matrix(axis='random',angle='random'):
         z = math.cos(theta)
     if angle == 'random':
     	angle = random.random() * 2 * math.pi
-    print("vector: "+str(x)+", "+str(y)+", "+str(z))
-    print("angle: "+str(angle))
+    else:
+        if radians is False:
+            angle *= 180./math.pi
     return pymatgen.core.operations.SymmOp.from_origin_axis_angle([0,0,0], [x,y,z], angle, angle_in_radians=True).rotation_matrix
 
-#Calculate the vector and angle from a matrix
 def matrix2aa(m):
+    '''
+    Return the axis and angle from a rotation matrix.
+    '''
+    #TODO: improve functionality with reflection and inversion matrices
     angle = math.acos(( m[0][0] + m[1][1] + m[2][2] - 1)/2)
     x = (m[2][1] - m[1][2])/np.sqrt((m[2][1] - m[1][2])**2+(m[0][2] - m[2][0])**2+(m[1][0] - m[0][1])**2)
     y = (m[0][2] - m[2][0])/np.sqrt((m[2][1] - m[1][2])**2+(m[0][2] - m[2][0])**2+(m[1][0] - m[0][1])**2)
     z = (m[1][0] - m[0][1])/np.sqrt((m[2][1] - m[1][2])**2+(m[0][2] - m[2][0])**2+(m[1][0] - m[0][1])**2)
     return [x,y,z], angle
-
-#Calculate the conjugation matrix
-#C = B A B^-1, A = C B C^-1
 
 #Test Functionality
 #----------------------------------------------------
