a4920dbc54006a108d212e029e5998a1ada7c525
Qiang Zhu <alecfans@gmail.com>
2018-08-06 14:25:28 +0800

    change setup.py based on distutil.setup format

--- setup.py
+++ setup.py
@@ -4,17 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    include_package_data=True,
     name="pyxtal",
     version="0.1dev",
     author="Scott Fredericks, Qiang Zhu",
     author_email="fredes3@unlv.nevada.edu",
-    description="Python code for ab initio generation of crystal structures based on symmetry constraints.",
+    description="Python code for generation of crystal structures based on symmetry constraints.",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    #long_description_content_type="text/markdown",
     url="https://github.com/qzhu2017/pyXtal",
     packages=['pyxtal', 'pyxtal.database'],
-    package_data={'pyxtal': ['pyxtal.database/*.csv']},
+    package_data={'pyxtal.database': ['*.csv']},
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
