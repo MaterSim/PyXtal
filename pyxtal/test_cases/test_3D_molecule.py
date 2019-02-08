e1fbc3679181e487e04925a12a5e69562c8a9d31
Qiang Zhu <alecfans@gmail.com>
2019-02-07 21:49:51 -0800

    add the N_attempts in output

--- pyxtal/test_cases/test_3D_molecule.py
+++ pyxtal/test_cases/test_3D_molecule.py
@@ -18,7 +18,8 @@ for mol in mols:
             rand_crystal = molecular_crystal(sg, [mol], [4], 1.6)
             if rand_crystal.valid:
                 run = False
-                print('Mol: {0:s}  SG: {1:d} Time: {2:.3f} seconds'.format(mol, sg, time()-start))
+                print('Mol: {0:s}  SG: {1:d} Time: {2:.3f} seconds, N_attempts: {3:4d}'.format(
+                    mol, sg, time()-start, rand_crystal.numattempts))
                 content = str(CifWriter(rand_crystal.struct, symprec=0.1))
                 with open(filename, 'a+') as f:
                     f.writelines(content)
