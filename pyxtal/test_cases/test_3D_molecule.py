3f38fb3285cda6423cf037bfbfefce45a8977cb9
Qiang Zhu <alecfans@gmail.com>
2019-02-07 22:09:09 -0800

    change the sequence in test_3D_molecule

--- pyxtal/test_cases/test_3D_molecule.py
+++ pyxtal/test_cases/test_3D_molecule.py
@@ -4,7 +4,7 @@ from time import time
 from pymatgen.io.cif import CifWriter
 import os
 
-mols = ['CH4', 'H2O', 'NH3', 'urea', 'benzene', 'C60', 'pentacene', 'roy', 'aspirin']
+mols = ['CH4', 'H2O', 'NH3', 'urea', 'benzene', 'roy', 'aspirin', 'pentacene', 'C60']
 filename = 'out.cif'
 if os.path.isfile(filename):
     os.remove(filename)
@@ -18,7 +18,7 @@ for mol in mols:
             rand_crystal = molecular_crystal(sg, [mol], [4], 1.6)
             if rand_crystal.valid:
                 run = False
-                print('Mol: {0:s}  SG: {1:d} Time: {2:.3f} seconds, N_attempts: {3:4d}'.format(
+                print('Mol: {0:10s}  SG: {1:d} Time: {2:.3f} seconds, N_attempts: {3:4d}'.format(
                     mol, sg, time()-start, rand_crystal.numattempts))
                 content = str(CifWriter(rand_crystal.struct, symprec=0.1))
                 with open(filename, 'a+') as f:
