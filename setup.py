b74bc59525a3f12f623981cbb52ea1932b87ca5f
Qiang Zhu <alecfans@gmail.com>
2019-10-25 22:46:17 -0700

    updaet setup

--- setup.py
+++ setup.py
@@ -30,7 +30,11 @@ setup(
                   #'pyxtal.potentials': ['*'],
                  },
 
-    scripts=['scripts/pyxtal', 'scripts/pyxtal_test', 'scripts/pyxtal_m'],
+    scripts=['scripts/pyxtal', 
+             'scripts/pyxtal_test', 
+             'scripts/pyxtal_symmetry',
+             'scripts/pyxtal_molecule',
+             ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
