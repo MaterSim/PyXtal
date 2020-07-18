fix the issue of rotation class

pyxtal/molecule.py
@@ -3,6 +3,10 @@ Module for handling molecules. Uses the pymatgen.core.structure.Molecule
 class as a base. Has a function for reorienting molecules
 (reoriented_molecule), and for calculating valid orientations within a Wyckoff
 position based on symmetry (orientation_in_wyckoff_position).
+The orientation class can be used to identify
+degrees of freedom for molecules in Wyckoff positions with certain symmetry
+constraints.
+
 """
 #Imports
 import numpy as np
@@ -14,7 +18,7 @@ from pymatgen.core.structure import Molecule
 from pymatgen.symmetry.analyzer import PointGroupAnalyzer, generate_full_symmops
 
 #PyXtal imports
-from pyxtal.operations import SymmOp, OperationAnalyzer, rotate_vector, angle, is_orthogonal
+from pyxtal.operations import SymmOp, OperationAnalyzer, rotate_vector, angle, printx
 from pyxtal.database.collection import Collection
 from pyxtal.constants import pi
 #Define functions
@@ -211,7 +215,9 @@ def get_symmetry(mol, already_oriented=False):
             symm_m.append(op)
         #Add 12-fold  and reflections in place of ininitesimal rotation
         for axis in [[1,0,0],[0,1,0],[0,0,1]]:
-            op = SymmOp.from_rotation_and_translation(aa2matrix(axis, pi/6), [0,0,0])
+            #op = SymmOp.from_rotation_and_translation(aa2matrix(axis, pi/6), [0,0,0])
+            m1 = Rotation.from_rotvec(pi/6*axis).as_matrix()
+            op = SymmOp.from_rotation_and_translation(m1, [0,0,0])
             if pga.is_valid_op(op):
                 symm_m.append(op)
                 #Any molecule with infinitesimal symmetry is linear;
@@ -279,6 +285,7 @@ def orientation_in_wyckoff_position(mol, wyckoff_position, randomize=True,
     #For single atoms, there are no constraints
     if len(mol) == 1:
         return [Orientation([[1,0,0],[0,1,0],[0,0,1]], degrees=2)]
+
     wyckoffs = wyckoff_position.ops
     w_symm = wyckoff_position.symmetry_m
     index = wyckoff_position.index
@@ -311,6 +318,7 @@ def orientation_in_wyckoff_position(mol, wyckoff_position, randomize=True,
             chiral = False
         elif opa.type == "inversion":
             chiral = False
+
     #If molecule is chiral and allow_inversion is False,
     #check if WP breaks symmetry
     if chiral is True:
@@ -319,6 +327,7 @@ def orientation_in_wyckoff_position(mol, wyckoff_position, randomize=True,
                 if np.linalg.det(op.rotation_matrix) < 0:
                     printx("Warning: cannot place chiral molecule in spagegroup", priority=2)
                     return False
+
     #Store OperationAnalyzer objects for each Wyckoff symmetry SymmOp
     opa_w = []
     for op_w in symm_w:
@@ -432,9 +441,12 @@ def orientation_in_wyckoff_position(mol, wyckoff_position, randomize=True,
                 if not np.isclose(a, 0, rtol=.01):
                     T2 = np.dot(np.linalg.inv(R), T)
                 a = angle(np.dot(T2, opa.axis), constraint2.axis)
-                if not np.isclose(a, 0, rtol=.01):
-                    printx("Error: Generated incorrect rotation: "+str(theta)+"\n"
-                    +"(Within orientation_in_wyckoff_position)", priority=0)
+                #if not np.isclose(a, 0, rtol=.01):
+                #    msg = "Error: Generated incorrect rotation: "
+                #    msg += str(theta)
+                #    msg += "\n(Within orientation_in_wyckoff_position)"
+                #    printx(msg, priority=0)
+
                 o = Orientation(T2, degrees=0)
                 orientations.append(o)
         #If there is only one constraint
@@ -455,8 +467,8 @@ def orientation_in_wyckoff_position(mol, wyckoff_position, randomize=True,
                 if i > j and j in list_j and j in list_i:
                     #m1 = o1.get_matrix(angle=0)
                     #m2 = o2.get_matrix(angle=0)
-                    m1 = o1.r.as_matrix()
-                    m2 = o2.r.as_matrix()
+                    m1 = o1.matrix
+                    m2 = o2.matrix
                     new_op = SymmOp.from_rotation_and_translation(np.dot(m2, np.linalg.inv(m1)), [0,0,0])
                     P = SymmOp.from_rotation_and_translation(np.linalg.inv(m1), [0,0,0])
                     old_op = P*new_op*P.inverse
@@ -508,9 +520,6 @@ class Orientation():
     """
 
     def __init__(self, matrix, degrees=0, axis=None):
-        if (not is_orthogonal(matrix)):
-            printx("Error: Supplied orientation matrix is not orthogonal", priority=1)
-            return
         if (degrees == 1) and (axis is None):
             printx("Error: Constraint vector required for orientation", priority=1)
         # scipy.spatial.transform.Rotation class
@@ -533,8 +542,6 @@ class Orientation():
                 random 3d rotation matrix to multiply by. If the original matrix
                 is wanted, set angle=0, or call self.matrix
 
-        Returns:
-            a 3x3 rotation (and/or inversion) matrix (numpy array)
         """
         if self.degrees == 2:
             if angle == "random":
@@ -546,7 +553,6 @@ class Orientation():
                 self.angle = angle
             self.r = Rotation.from_rotvec(self.angle*self.axis)
             self.matrix = self.r.as_matrix()
-            return self.matrix
 
         elif self.degrees == 1:
             if angle == "random":
@@ -554,8 +560,37 @@ class Orientation():
             self.angle = angle
             self.r = Rotation.from_rotvec(self.angle*self.axis)
             self.matrix = self.r.as_matrix()
-            return self.matrix
             
+
+    def get_matrix(self, angle="random"):
+        """
+        Generate a 3x3 rotation matrix consistent with the orientation's
+        constraints. Allows for specification of an angle (possibly random) to
+        rotate about the constraint axis.
+
+        Args:
+            angle: an angle to rotate about the constraint axis. If "random",
+                chooses a random rotation angle. If self.degrees==2, chooses a
+                random 3d rotation matrix to multiply by. If the original matrix
+                is wanted, set angle=0, or call self.matrix
+
+        Returns:
+            a 3x3 rotation (and/or inversion) matrix (numpy array)
+        """
+        if self.degrees == 2:
+            if angle == "random":
+                axis = np.random.sample(3)
+                axis = axis/np.linalg.norm(axis)
+                angle = np.random.random()*pi*2
+            else:
+                axis = self.axis
+            return Rotation.from_rotvec(angle*axis).as_matrix()
+
+        elif self.degrees == 1:
+            if angle == "random":
+                angle = np.random.random()*pi*2
+            return Rotation.from_rotvec(angle*self.axis).as_matrix()
+
         elif self.degrees == 0:
             return self.matrix
 
@@ -643,23 +678,3 @@ class Orientation():
         self.change_orientation()
         return self
 
-
-#Test Functionality
-if __name__ == "__main__":
-#---------------------------------------------------
-
-    #Testing water
-    mol = Collection('molecules')['H2O']
-    print("Original molecule:")
-    print(mol)
-    print()
-    #Apply random rotation to avoid lucky results
-    R = aa2matrix(1,1,random=True)
-    R_op = SymmOp.from_rotation_and_translation(R,[0,0,0])
-    mol.apply_operation(R_op)
-    print("Rotated molecule:")
-    print(mol)
-    print()
-
-    #pga = PointGroupAnalyzer(mol)
-    #mol = pga.symmetrize_molecule()['sym_mol']    

pyxtal/operations.py
@@ -12,11 +12,10 @@ constraints.
 import numpy as np
 from copy import deepcopy
 from warnings import warn
+from scipy.spatial.distance import cdist
 from scipy.spatial.transform import Rotation 
 
 #External Libraries
-import numpy as np
-from scipy.spatial.distance import cdist
 from pymatgen.core.operations import SymmOp
 from pyxtal.tolerance import Tol_matrix
 from pyxtal.constants import pi, rad, deg, pyxtal_verbosity
@@ -584,6 +583,53 @@ def is_orthogonal(m, tol=.001):
     else:
         return True
 
+def aa2matrix(axis, angle, radians=True, random=False):
+    """
+    Given an axis and an angle, return a 3x3 rotation matrix.
+    Based on:
+    https://en.wikipedia.org/wiki/Rotation_matrix#Axis_and_angle
+
+    Args:
+        axis: a vector about which to perform a rotation
+        angle: the angle of rotation
+        radians: whether the supplied angle is in radians (True)
+            or in degrees (False)
+        random: whether or not to choose a random rotation matrix. If True, the
+            axis and angle are ignored, and a random orientation is generated
+
+    Returns:
+        a 3x3 numpy array representing a rotation matrix
+    """
+    #Convert to radians if necessary
+    if radians is not True:
+        angle *= rad
+    #Allow for generation of random rotations
+    if random is True:
+        #a = np.random.random()
+        axis = np.random.sample(3)
+        angle = np.random.random()*pi*2
+    #Ensure axis is a unit vector
+    axis = axis / np.linalg.norm(axis)
+    #Define quantities which are reused
+    x = np.real(axis[0])
+    y = np.real(axis[1])
+    z = np.real(axis[2])
+    c = np.cos(angle)
+    s = np.sin(angle)
+    C = 1 - c
+    #Define the rotation matrix
+    Q = np.zeros([3,3])
+    Q[0][0] = x*x*C + c
+    Q[0][1] = x*y*C - z*s
+    Q[0][2] = x*z*C + y*s
+    Q[1][0] = y*x*C + z*s
+    Q[1][1] = y*y*C + c
+    Q[1][2] = y*z*C - x*s
+    Q[2][0] = z*x*C - y*s
+    Q[2][1] = z*y*C + x*s
+    Q[2][2] = z*z*C + c
+    return Q
+
 def rotate_vector(v1, v2):
     #TODO: Verify that multiplication order is correct
     #(matrix should come after vector in np.dot)
@@ -729,9 +775,12 @@ class OperationAnalyzer(SymmOp):
             if np.linalg.det(self.m) > 0:
                 self.inverted = False
                 rotvec = Rotation.from_matrix(self.m).as_rotvec()
-                self.angle = np.linalg.norm(rotvec)
-                self.axis = rotvec/self.angle
-                #self.axis, self.angle = matrix2aa(self.m)
+                if np.sum(rotvec.dot(rotvec)) < 1e-6:
+                    self.axis = None
+                    self.angle = 0
+                else:
+                    self.angle = np.linalg.norm(rotvec)
+                    self.axis = rotvec/self.angle
                 if np.isclose(self.angle, 0):
                     self.type = "identity"
                     """The type of operation. Is one of 'identity', 'inversion',
@@ -752,10 +801,14 @@ class OperationAnalyzer(SymmOp):
             #If determinant is negative
             elif np.linalg.det(self.m)< 0:
                 self.inverted = True
-                rotvec = Rotation.from_matrix(-self.m).as_rotvec()
-                self.angle = np.linalg.norm(rotvec)
-                self.axis = rotvec/self.angle
-                #self.axis, self.angle = matrix2aa(mi)
+                rotvec = Rotation.from_matrix(-1*self.m).as_rotvec()
+                if np.sum(rotvec.dot(rotvec)) < 1e-6:
+                    self.axis = None
+                    self.angle = 0
+                else:
+                    self.angle = np.linalg.norm(rotvec)
+                    self.axis = rotvec/self.angle
+
                 if np.isclose(self.angle, 0):
                     self.type = "inversion"
                     self.order = int(2)
@@ -839,76 +892,7 @@ class OperationAnalyzer(SymmOp):
             opa1 = OperationAnalyzer(op1)
         return opa1.is_conjugate(op2)
 
-def matrix2aa(m, radians=True):
-    """
-    Return the axis and angle from a rotation matrix. m must be an orthogonal
-    matrix with determinant 1. The axis is an eigenvector with eigenvalue 1.
-    The angle is determined by the trace and the asymmetryic part of m.
-    Based on:
-    https://en.wikipedia.org/wiki/Rotation_matrix#Axis_and_angle
-    Args:
-        m: an orthogonal 3x3 matrix (list, array, or matrix) with determinant 1
-        radians: whether the returned angle is in radians (True)
-            or in degrees (False)
-    Returns:
-        [axis, angle]: the axis (3-vector) and angle (in radians) of the supplied rotation matrix
-    """
-    if type(m) == SymmOp:
-        m = m.rotation_matrix
-    #Check if m is the identity matrix
-    if np.allclose(m, np.identity(3)):
-        return None, 0.
-    if not is_orthogonal(m):
-        printx("Error: matrix is not orthogonal.", priority=1)
-        return
-    #Check that m has posititve determinant
-    if not np.isclose(np.linalg.det(m), 1, rtol=.001):
-        printx("Error: invalid rotation matrix. Determinant is not 1.\n"
-            +"Divide matrix by inversion operation beore calling matrix2aa.", priority=1)
-        return
-    #Determine the eigenvector(s) of m
-    e = np.linalg.eig(m)
-    eigenvalues = e[0]
-    possible = np.transpose(e[1])
-    eigenvectors = []
-    for v in possible:
-        if np.allclose(v, np.dot(m, v)):
-            eigenvectors.append(v)
-    #Determine the angle of rotation
-    if len(eigenvectors) == 1:
-        v = eigenvectors[0]
-        x = m[2][1] - m[1][2]
-        y = m[0][2] - m[2][0]
-        z = m[1][0] - m[0][1]
-        r = np.sqrt(x**2+y**2+z**2)
-        t = m[0][0] + m[1][1] + m[2][2]
-        theta = np.arctan2(r, t-1.)
-        #Ensure 0<theta<pi
-        if theta > pi:
-            #Make sure 180 degree rotations are not converted to 0
-            if np.isclose(theta, pi, atol=1e-2, rtol=1e-3):
-                theta = pi
-            else:
-                theta = pi*2 - theta
-        if theta < 0:
-            theta *= -1
-            v *= -1
-        #Convert to degrees if necessary
-        if radians is not True:
-            theta *= deg
-        return v, theta
-    #If no eigenvectors are found
-    elif len(eigenvectors) == 0:
-        printx("Error: matrix2aa did not find any eigenvectors.", priority=1)
-        return
-    #If multiple eigenvectors are found
-    elif len(eigenvectors) > 1:
-        printx("Warning: multiple eigenvectors found.\n"
-            +"Found eigenvectors:\n"
-            +str(eigenvectors), priority=1)
-        return None, 0.
-
-
+        
 #Test Functionality
 if __name__ == "__main__":
 #----------------------------------------------------