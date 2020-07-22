2decd7b465c0850c7d51baae6edf2ace1fbc6f95
Rhys Goodall <reag2@cam.ac.uk>
2020-07-22 13:01:12 +0100

    fix: L in logo

--- pyxtal/__init__.py
+++ pyxtal/__init__.py
@@ -1,22 +1,26 @@
 from pyxtal.version import __version__
+
 name = "pyxtal"
+
 
 def print_logo():
     """
     print out the logo and version information
     """
-    
-    print("""
+
+    print(
+        """
              ______       _    _          _   
             (_____ \     \ \  / /        | |   
              _____) )   _ \ \/ / |_  ____| |  
             |  ____/ | | | )  (|  _)/ _  | | 
-            | |    | |_| |/ /\ \ |_( ( | | |___
-            |_|     \__  /_/  \_\___)_||_|_(___
-                   (____/      """)
-    print('\n')
-    print('----------------------(version', __version__,')----------------------\n')
-    print('A Python package for random crystal generation')
-    print('The source code is available at https://github.com/qzhu2017/pyxtal')
-    print('Developed by Zhu\'s group at University of Nevada Las Vegas\n\n')
+            | |    | |_| |/ /\ \ |_( (_| | |___
+            |_|     \__  /_/  \_\___)__|_|_____)
+                   (____/      """
+    )
+    print("\n")
+    print("----------------------(version", __version__, ")----------------------\n")
+    print("A Python package for random crystal generation")
+    print("The source code is available at https://github.com/qzhu2017/pyxtal")
+    print("Developed by Zhu's group at University of Nevada Las Vegas\n\n")
 
