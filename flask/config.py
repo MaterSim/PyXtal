d299fd2f51edab21bc03b8bbd4ed22b9376df2fa
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2020-07-31 19:02:49 -0700

    Add configs for session and error traceback emails

---
+++ flask/config.py
@@ -0,0 +1,11 @@
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['vxrd.info@gmail.com']
