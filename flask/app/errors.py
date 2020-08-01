7ca3c0c37b2adcf303e8f523de258cdad4fd35da
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2020-07-31 18:58:32 -0700

    Add core Flask app files

---
+++ flask/app/errors.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from app import app
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+    
