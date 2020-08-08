6881ace4ab289841bb64c556162aac79fdbdca30
Qiang Zhu <alecfans@gmail.com>
2020-08-07 20:56:55 -0700

    move the check_compatible function inside

--- pyxtal/crystal.py
+++ pyxtal/crystal.py
@@ -4,13 +4,9 @@ import random
 import numpy as np
 from copy import deepcopy
 
-# External Libraries
-from pymatgen.core.structure import Structure, Molecule
-
 # PyXtal imports #avoid *
 from pyxtal.symmetry import Group, choose_wyckoff
 from pyxtal.wyckoff_site import atom_site, check_atom_sites, WP_merge
-from pyxtal.io import write_cif
 from pyxtal.msg import printx
 from pyxtal.tolerance import Tol_matrix
 from pyxtal.lattice import Lattice, cellsize, add_vacuum
@@ -18,115 +14,6 @@ from pyxtal.database.element import Element
 
 # Define functions
 # ------------------------------
-def check_compatible(group, numIons):
-    """
-    Checks if the number of atoms is compatible with the Wyckoff
-    positions. Considers the number of degrees of freedom for each Wyckoff
-    position, and makes sure at least one valid combination of WP's exists.
-
-    NOTE Comprhys: Is degrees of freedom used symnomously with multiplicity?
-    perhaps standardising to multiplicity would be clearer?
-    """
-    # Store whether or not at least one degree of freedom exists
-    has_freedom = False
-    # Store the wp's already used that don't have any freedom
-    used_indices = []
-    # Loop over species
-    for numIon in numIons:
-        # Get lists of multiplicity, maxn and freedom
-        l_mult0 = []
-        l_maxn0 = []
-        l_free0 = []
-        indices0 = []
-        for i_wp, wp in enumerate(group):
-            indices0.append(i_wp)
-            l_mult0.append(len(wp))
-            l_maxn0.append(numIon // len(wp))
-            if np.allclose(wp[0].rotation_matrix, np.zeros([3, 3])):
-                l_free0.append(False)
-            else:
-                l_free0.append(True)
-        # Remove redundant multiplicities:
-        l_mult = []
-        l_maxn = []
-        l_free = []
-        indices = []
-        for mult, maxn, free, i_wp in zip(l_mult0, l_maxn0, l_free0, indices0):
-            if free is True:
-                if mult not in l_mult:
-                    l_mult.append(mult)
-                    l_maxn.append(maxn)
-                    l_free.append(True)
-                    indices.append(i_wp)
-            elif free is False and i_wp not in used_indices:
-                l_mult.append(mult)
-                indices.append(i_wp)
-                if mult <= numIon:
-                    l_maxn.append(1)
-                elif mult > numIon:
-                    l_maxn.append(0)
-                l_free.append(False)
-
-        # Loop over possible combinations
-        p = 0  # Create pointer variable to move through lists
-
-        # Store the number of each WP, used across possible WP combinations
-        n0 = [0] * len(l_mult)
-        n = deepcopy(n0)
-        for i, mult in enumerate(l_mult):
-            if l_maxn[i] != 0:
-                p = i
-                n[i] = l_maxn[i]
-                break
-        p2 = p
-        if n == n0:
-            return False
-        while True:
-            num = np.dot(n, l_mult)
-            dobackwards = False
-            # The combination works: move to next species
-            if num == numIon:
-                # Check if at least one degree of freedom exists
-                for val, free, i_wp in zip(n, l_free, indices):
-                    if val > 0:
-                        if free is True:
-                            has_freedom = True
-                        elif free is False:
-                            used_indices.append(i_wp)
-                break
-            # All combinations failed: return False
-            if n == n0 and p >= len(l_mult) - 1:
-                return False
-            # Too few atoms
-            if num < numIon:
-                # Forwards routine
-                # Move p to the right and max out
-                if p < len(l_mult) - 1:
-                    p += 1
-                    n[p] = min((numIon - num) // l_mult[p], l_maxn[p])
-                elif p == len(l_mult) - 1:
-                    # p is already at last position: trigger backwards routine
-                    dobackwards = True
-            # Too many atoms
-            if num > numIon or dobackwards is True:
-                # Backwards routine
-                # Set n[p] to 0, move p backwards to non-zero, and decrease by 1
-                n[p] = 0
-                while p > 0 and p > p2:
-                    p -= 1
-                    if n[p] != 0:
-                        n[p] -= 1
-                        if n[p] == 0 and p == p2:
-                            p2 = p + 1
-                        break
-    # All species passed: return True
-    if has_freedom is True:
-        return True
-    # All species passed, but no degrees of freedom: return 0
-    elif has_freedom is False:
-        return 0
-
-
 class random_crystal:
     """
     Class for storing and generating atomic crystals based on symmetry
@@ -306,6 +193,115 @@ class random_crystal:
 
         self.generate_crystal()
 
+    def check_compatible(self, group, numIons):
+        """
+        Checks if the number of atoms is compatible with the Wyckoff
+        positions. Considers the number of degrees of freedom for each Wyckoff
+        position, and makes sure at least one valid combination of WP's exists.
+    
+        NOTE Comprhys: Is degrees of freedom used symnomously with multiplicity?
+        perhaps standardising to multiplicity would be clearer?
+        """
+        # Store whether or not at least one degree of freedom exists
+        has_freedom = False
+        # Store the wp's already used that don't have any freedom
+        used_indices = []
+        # Loop over species
+        for numIon in numIons:
+            # Get lists of multiplicity, maxn and freedom
+            l_mult0 = []
+            l_maxn0 = []
+            l_free0 = []
+            indices0 = []
+            for i_wp, wp in enumerate(group):
+                indices0.append(i_wp)
+                l_mult0.append(len(wp))
+                l_maxn0.append(numIon // len(wp))
+                if np.allclose(wp[0].rotation_matrix, np.zeros([3, 3])):
+                    l_free0.append(False)
+                else:
+                    l_free0.append(True)
+            # Remove redundant multiplicities:
+            l_mult = []
+            l_maxn = []
+            l_free = []
+            indices = []
+            for mult, maxn, free, i_wp in zip(l_mult0, l_maxn0, l_free0, indices0):
+                if free is True:
+                    if mult not in l_mult:
+                        l_mult.append(mult)
+                        l_maxn.append(maxn)
+                        l_free.append(True)
+                        indices.append(i_wp)
+                elif free is False and i_wp not in used_indices:
+                    l_mult.append(mult)
+                    indices.append(i_wp)
+                    if mult <= numIon:
+                        l_maxn.append(1)
+                    elif mult > numIon:
+                        l_maxn.append(0)
+                    l_free.append(False)
+    
+            # Loop over possible combinations
+            p = 0  # Create pointer variable to move through lists
+    
+            # Store the number of each WP, used across possible WP combinations
+            n0 = [0] * len(l_mult)
+            n = deepcopy(n0)
+            for i, mult in enumerate(l_mult):
+                if l_maxn[i] != 0:
+                    p = i
+                    n[i] = l_maxn[i]
+                    break
+            p2 = p
+            if n == n0:
+                return False
+            while True:
+                num = np.dot(n, l_mult)
+                dobackwards = False
+                # The combination works: move to next species
+                if num == numIon:
+                    # Check if at least one degree of freedom exists
+                    for val, free, i_wp in zip(n, l_free, indices):
+                        if val > 0:
+                            if free is True:
+                                has_freedom = True
+                            elif free is False:
+                                used_indices.append(i_wp)
+                    break
+                # All combinations failed: return False
+                if n == n0 and p >= len(l_mult) - 1:
+                    return False
+                # Too few atoms
+                if num < numIon:
+                    # Forwards routine
+                    # Move p to the right and max out
+                    if p < len(l_mult) - 1:
+                        p += 1
+                        n[p] = min((numIon - num) // l_mult[p], l_maxn[p])
+                    elif p == len(l_mult) - 1:
+                        # p is already at last position: trigger backwards routine
+                        dobackwards = True
+                # Too many atoms
+                if num > numIon or dobackwards is True:
+                    # Backwards routine
+                    # Set n[p] to 0, move p backwards to non-zero, and decrease by 1
+                    n[p] = 0
+                    while p > 0 and p > p2:
+                        p -= 1
+                        if n[p] != 0:
+                            n[p] -= 1
+                            if n[p] == 0 and p == p2:
+                                p2 = p + 1
+                            break
+        # All species passed: return True
+        if has_freedom is True:
+            return True
+        # All species passed, but no degrees of freedom: return 0
+        elif has_freedom is False:
+            return 0
+    
+
 
     def check_consistency(self, site, numIon):
         num = 0
@@ -400,6 +396,7 @@ class random_crystal:
                     fmt = 'cif'
 
             if fmt == "cif":
+                from pyxtal.io import write_cif
                 return write_cif(self, filename, "from_pyxtal", permission)
             else:
                 pmg_struc = self.to_pymatgen()
@@ -425,12 +422,6 @@ class random_crystal:
     def __repr__(self):
         return str(self)
 
-    def print_all(self):
-        """
-        Prints useful information about the generated crystal.
-        """
-        print(str(self))
-
     def show(self, **kwargs):
         """
         display the crystal structure
@@ -448,7 +439,7 @@ class random_crystal:
        """
         # Check the minimum number of degrees of freedom within the Wyckoff positions
         self.numattempts = 1
-        degrees = check_compatible(self.group, self.numIons)
+        degrees = self.check_compatible(self.group, self.numIons)
         if degrees is False:
             printx(self.Msg1, priority=1)
             self.valid = False
@@ -520,11 +511,11 @@ class random_crystal:
         extract the coordinates and species information 
 
         Args:
-        abosulte: if True, return the cartesian coords otherwise fractional
+            abosulte: if True, return the cartesian coords otherwise fractional
 
         Returns:
-        total_coords: N*3 numpy array 
-        species: N-length list, e.g. ["C", "C", ...]
+            total_coords: N*3 numpy array 
+            species: N-length list, e.g. `["C", "C", ...]`
         """
         species = []
         total_coords = None
@@ -561,6 +552,7 @@ class random_crystal:
         """
         export to Pymatgen structure object
         """
+        from pymatgen.core.structure import Structure, Molecule
 
         if self.valid:
             if self.dim > 0:
