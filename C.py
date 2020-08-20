442674a72b54e46ab8309a9336b8bd88177a9e98
qzhu2017 <alecfans@gmail.com>
2020-08-20 08:59:48 -0700

    update C.py

--- C.py
+++ C.py
@@ -7,8 +7,9 @@ from spglib import get_symmetry_dataset
 file = "C-POSCARs"
 if os.path.exists(file):
     os.remove(file)
-for i in range(100):
-    struc = random_crystal(19, ["C"], [16], 1.0)
+for i in range(10):
+    #struc = random_crystal(194, ["C"], [4], 1.0)
+    struc = random_crystal(227, ["C"], [2], 1.0)
     if struc.valid:
         calc = GULP(struc, ff="tersoff.lib")
         calc.run()
@@ -16,6 +17,6 @@ for i in range(100):
         info = get_symmetry_dataset(s, symprec=1e-1)
         s.write("1.vasp", format='vasp', vasp5=True, direct=True)
         os.system("cat 1.vasp >> " + file)
-        print("{:4d} {:8.3f} {:s}".format(i, calc.energy, info['international']))
+        print("{:4d} {:8.3f} {:s}".format(i, calc.energy/8, info['international']))
         #print(calc.stress)
         #print(calc.cell)
