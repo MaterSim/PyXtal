093c5fa5384eee31fefcb00ced09fb741d46ceac
Qiang Zhu <alecfans@gmail.com>
2020-11-09 20:31:05 -0800

    update doc

--- doc/conf.py
+++ doc/conf.py
@@ -24,9 +24,9 @@ copyright = "2018, Qiang Zhu, Scott Fredericks"
 author = "Qiang Zhu, Scott Fredericks"
 
 # The short X.Y version
-version = "0.1.0"
+version = "0.1.2"
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.1.2"
 
 
 # -- General configuration ---------------------------------------------------
