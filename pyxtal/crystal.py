d4c4b00c87f3c3a5f6e4953552229acb84b62aa2
Qiang Zhu <alecfans@gmail.com>
2020-11-11 13:40:33 -0800

    fix a bug when the site is given

--- pyxtal/crystal.py
+++ pyxtal/crystal.py
@@ -681,6 +681,7 @@ class random_crystal:
                 # If site the pre-assigned, do not accept merge
                 if wp is not False:
                     if site is not None and mult != wp.multiplicity:
+                        cycle += 1
                         continue 
                     # Use a Wyckoff_site object for the current site
                     new_site = atom_site(wp, pt, specie)
