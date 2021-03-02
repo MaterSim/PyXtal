e7dbf52c433aa70c0563fc3a6740d0c95f33d776
qzhu2017 <alecfans@gmail.com>
2021-03-01 19:38:23 -0800

    update the notebook and fix some tiny issues

--- pyxtal/miscellaneous/single_check.py
+++ pyxtal/miscellaneous/single_check.py
@@ -12,21 +12,22 @@ from pymatgen import Structure
 from pyxtal import pyxtal
 from pyxtal.supergroup import supergroups
 
-paras = (["R-3", 147, 'k'],
+paras = (
+         ["P4_332", 155, 't'], #1-4 splitting
+         ["I-43m", 160, 't'], #mapping
+         ['I4_132',98,'t'], #mapping
+         #["P4_332", 96, 't'],
+         #["R-3", 147, 'k'],
          #["P3_112", 5, 't'],
-         # ["P6_422", 21, 't'],
-         # ["P4_332", 155, 't'],
-         # ["P4_332", 96, 't'],
-         # ["FD3", 70, 't'], #needs 1->3
-         # ["Fd3m", 166, 't'],
-         # ["R-3c", 15, 't'],
-         # ["R32", 5, 't'],
-         # ["Pm3", 47, 't'], #needs 1->3
+         #["P6_422", 21, 't'],
+         #["FD3", 70, 't'], 
+         #["Fd3m", 166, 't'],
+         #["R-3c", 15, 't'],
+         #["R32", 5, 't'],
+         #["Pm3", 47, 't'], 
          #["Pmma", 25, 't'],
-         #["I-43m", 160, 't'], #mapping
-         #['I4_132',98,'t'], #mapping
-
         )
+
 for para in paras:
     name, H, gtype = para
     name = "pyxtal/miscellaneous/cifs/" + name + ".cif"
@@ -36,7 +37,7 @@ for para in paras:
     pmg_s1 = s.to_pymatgen()
     G = s.group.number
     for i in range(10):
-        struc_h = s.subgroup_once(eps=0.15, H=H, group_type=gtype, mut_lat=False, max_cell=3)
+        struc_h = s.subgroup_once(eps=0.15, H=H, group_type=gtype, mut_lat=False, max_cell=4)
         print(struc_h)
         print(s)
         sup = supergroups(struc_h, G=G, d_tol=0.2, show=True, max_per_G=500)
