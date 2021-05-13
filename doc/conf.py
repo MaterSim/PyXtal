d48e32366e2ebf543a633095b887896c68e971f1
Qiang Zhu <alecfans@gmail.com>
2021-05-13 01:17:22 -0700

    prep for v0.2.6

--- doc/conf.py
+++ doc/conf.py
@@ -24,9 +24,9 @@ copyright = "2018-2021, Qiang Zhu, Scott Fredericks, Kevin Parrish"
 author = "Qiang Zhu, Scott Fredericks, Kevin Parrish"
 
 # The short X.Y version
-version = "0.2.5"
+version = "0.2.6"
 # The full version, including alpha/beta/rc tags
-release = "0.2.5"
+release = "0.2.6"
 
 
 # -- General configuration ---------------------------------------------------
