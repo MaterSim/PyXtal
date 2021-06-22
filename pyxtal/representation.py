af74563448ac6668856a1eaaf40a62af002792a7
Qiang Zhu <alecfans@gmail.com>
2021-06-21 23:44:35 -0700

    extend 1D rep class to support Zp>1

--- pyxtal/representation.py
+++ pyxtal/representation.py
@@ -4,15 +4,27 @@ from pyxtal import pyxtal
 from pyxtal.symmetry import Group
 from pyxtal.lattice import Lattice
 from pyxtal.wyckoff_site import mol_site
+from pyxtal.molecule import find_id_from_smile
  
 class representation():
     """
     A class to handle the 1D representation of molecular crystal   
-    Only works for Zprime = 1
+    Works for Zprime > 1
+
+    Args:
+        x: a list of [cell, site_1, site_2, ...]
+        smiles: a list of [smile_1, smile_2, ...]
     """
 
     def __init__(self, x, smiles=None):
-        self.smiles = smiles
+        if smiles is not None:
+            self.smiles = []
+            for i, smile in enumerate(smiles):
+                if smile.endswith('.smi'): 
+                    smile = smile[:-4]
+                self.smiles.append(smile)
+        else:
+            self.smiles = None
         self.x = x
 
     @classmethod
@@ -22,13 +34,50 @@ class representation():
         """
         symmetry = [struc.group.number, struc.diag]
         lat = struc.lattice.encode()
-        vector = symmetry + lat
+        vector = [symmetry + lat]
+        smiles = []
         for site in struc.mol_sites:
-            vector += site.encode()
-            smiles = [site.molecule.smile]
+            vector.append(site.encode())
+            smiles.append(site.molecule.smile)
         x = vector
         return cls(x, smiles)
     
+    @classmethod
+    def from_string(cls, inputs, smiles):
+        """
+        Convert the string into 1D vector
+
+        Args:
+            inputs: input string 
+            smiles: list of smiles
+        """
+        #parse the cell
+        inputs = [float(tmp) for tmp in inputs.split()]
+        g, diag = int(inputs[0]), int(inputs[1])
+        if g<=2:
+            n_cell = 8
+        elif g<=15:
+            n_cell = 6
+        elif g<=74:
+            n_cell = 5
+        cell = [g, diag] + inputs[2:n_cell]
+        
+        x = [cell]
+        n_site = int(inputs[n_cell])
+        if n_site != len(smiles):
+            raise ValueError("Inconsistent number of molecules")
+        n_cell += 1
+
+        for i, smile in enumerate(smiles):
+            if smile.endswith('.smi'): 
+                smile=smile[:-4]
+            n_torsion = len(find_id_from_smile(smile))
+            n_mol = 7 + n_torsion
+            inputs[n_cell+n_mol-1] = int(inputs[n_cell+n_mol-1])
+            x.append(inputs[n_cell:n_cell+n_mol])
+            n_cell += n_mol
+        return cls(x, smiles)
+ 
     def to_pyxtal(self, smiles=None):
         """
         convert to the pyxtal structure
@@ -36,84 +85,77 @@ class representation():
         if smiles is None:
             smiles = self.smiles
 
-        if smiles is None:
-            raise ValueError("no smiles")
-        else:
-            v = self.x
-            if type(v) != list:
-                v = list(v)
-            v[0] = int(v[0])
-            v[1] = bool(v[1])
-            v[-1] = bool(v[-1])
-            # symmetry
-            struc = pyxtal(molecular=True)
-            struc.group, struc.diag = Group(v[0]), v[1]
+        # symmetry
+        v = self.x[0]
+        struc = pyxtal(molecular=True)
+        struc.group, number, struc.diag = Group(v[0]), v[0], v[1]
     
-            # lattice
-            count = 2
-            ltype = struc.group.lattice_type
-            if ltype == 'triclinic':
-                a, b, c, alpha, beta, gamma = v[2], v[3], v[4], v[5], v[6], v[7]
-                count += 6
-            elif ltype == 'monoclinic':
-                a, b, c, alpha, beta, gamma = v[2], v[3], v[4], 90, v[5], 90
-                count += 4
-            elif ltype == 'orthorhombic':
-                a, b, c, alpha, beta, gamma = v[2], v[3], v[4], 90, 90, 90
-                count += 3
-            struc.lattice = Lattice.from_para(a, b, c, alpha, beta, gamma, ltype=ltype)
+        # lattice
+        ltype = struc.group.lattice_type
+        if ltype == 'triclinic':
+            a, b, c, alpha, beta, gamma = v[2], v[3], v[4], v[5], v[6], v[7]
+        elif ltype == 'monoclinic':
+            a, b, c, alpha, beta, gamma = v[2], v[3], v[4], 90, v[5], 90
+        elif ltype == 'orthorhombic':
+            a, b, c, alpha, beta, gamma = v[2], v[3], v[4], 90, 90, 90
+        struc.lattice = Lattice.from_para(a, b, c, alpha, beta, gamma, ltype=ltype)
     
-            # sites
-            struc.numMols = [] 
-            struc.molecules = []
-            struc.mol_sites = [] 
+        # sites
+        struc.numMols = [] 
+        struc.molecules = []
+        struc.mol_sites = [] 
 
-            for i, smile in enumerate(smiles):
-                dicts = {}
-                dicts['smile'] = smile
-                dicts['dim'] = 3
-                dicts['PBC'] = [1, 1, 1]
-                dicts['number'] = v[0]
-                dicts['diag'] = int(v[1])
-                dicts['index'] = 0
-                dicts['lattice'] = struc.lattice.matrix
-                dicts['lattice_type'] = ltype
-                dicts['center'] = v[count:count+3]
-                dicts['orientation'] = np.array(v[count+3:count+6])
-                dicts['rotor'] = v[count+6:-1]
-                dicts['reflect'] = int(v[-1])
-                site = mol_site.from_1D_dicts(dicts)
-                struc.mol_sites.append(site)
-                struc.molecules.append(site.molecule)
-                struc.numMols.append(site.wp.multiplicity)
+        for i, smile in enumerate(smiles):
+            if smile.endswith('.smi'): 
+                smile=smile[:-4]
+            v = self.x[i+1]
+            dicts = {}
+            dicts['smile'] = smile
+            dicts['dim'] = 3
+            dicts['PBC'] = [1, 1, 1]
+            dicts['number'] = number
+            dicts['diag'] = struc.diag
+            dicts['index'] = 0
+            dicts['lattice'] = struc.lattice.matrix
+            dicts['lattice_type'] = ltype
+            dicts['center'] = v[:3]
+            dicts['orientation'] = np.array(v[3:6])
+            dicts['rotor'] = v[6:-1]
+            dicts['reflect'] = int(v[-1])
+            site = mol_site.from_1D_dicts(dicts)
+            struc.mol_sites.append(site)
+            struc.molecules.append(site.molecule)
+            struc.numMols.append(site.wp.multiplicity)
 
-            struc._get_formula()
-            struc.source = '1D rep.'
-            struc.valid = True
+        struc._get_formula()
+        struc.source = '1D rep.'
+        struc.valid = True
 
-            return struc
+        return struc
     
     def to_string(self, time=None, eng=None, tag=None):
         """
         string representation
         """
         x = self.x
-        strs = "{:3d} {:d} ".format(int(x[0]), int(x[1]))
-        if int(x[0]) <=2:
-            num = 11
-        elif int(x[0]) <=15:
-            num = 9
-        elif int(x[0]) <=74:
-            num = 8
-            
-        for v in x[2:num]:
-            if v>99.9999:
-                strs += "{:5.1f} ".format(v)      
+        strs = "{:3d} {:d} ".format(int(x[0][0]), int(x[0][1]))
+
+        # data for cell
+        for c in x[0][2:]:
+            if c-100 > 1e-8:
+                strs += "{:5.1f} ".format(c)
             else:
-                strs += "{:5.2f} ".format(v)      
-        for v in x[num:-1]:
-            strs += "{:6.1f} ".format(v)      
-        strs += "{:d} ".format(int(x[-1]))      
+                strs += "{:5.2f} ".format(c)
+        
+        # data for molecule
+        strs += "{:d} ".format(len(self.smiles))
+        for i in range(len(self.smiles)):
+            for v in x[i+1][:-1]:
+                if v-100>1e-8:
+                    strs += "{:5.1f} ".format(v)      
+                else:
+                    strs += "{:5.2f} ".format(v)      
+            strs += "{:d} ".format(int(x[i+1][-1]))
 
         if time is not None:
             strs += "{:5.2f}".format(time)
@@ -127,17 +169,20 @@ class representation():
         return strs
 
 
+
 if __name__ == "__main__":
 
     #aspirin
     smiles = ['CC(=O)OC1=CC=CC=C1C(=O)O']
-    x = [14,False,11.43,6.49,11.19,83.31,0.77,0.57,0.53,48.55,24.31,145.94,-77.85,-4.40,170.86,False]
-    rep0 = representation(x)
-    print(rep0.to_string())
+    x = [[14,False,11.43,6.49,11.19,83.31],[0.77,0.57,0.53,48.55,24.31,145.94,-77.85,-4.40,170.86,False]]
+    #rep0 = representation(x, smiles)
+    #print(rep0.to_string())
     rep1 = representation(x, smiles)
     xtal = rep1.to_pyxtal()
     print(xtal)
     rep2 = representation.from_pyxtal(xtal)
     print(rep2.to_pyxtal())
     print(rep2.to_string())
-
+    string = "14 0 11.43  6.49 11.19 83.31 1  0.77  0.57  0.53 48.55 24.31 145.9 -77.85 -4.40 170.9 0"
+    rep3 = representation.from_string(string, smiles)
+    print(rep3.to_pyxtal())
