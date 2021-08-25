7c06602f85e2d34271d2e05fecfb467f76cf3a23
qzhu2017 <alecfans@gmail.com>
2021-08-25 15:37:18 -0700

    update version number

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     ],
     install_requires=[
         "spglib>=1.10.4",
-        "pymatgen>=2020.1.28",
+        "pymatgen>=2022.0.12",
         "pandas>=0.24.2",
         "networkx>=2.3",
         "py3Dmol>=0.8.0",
