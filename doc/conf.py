b6e86ec4df79271262b1766ca2c7869f97e6b9d6
qzhu2017 <alecfans@gmail.com>
2021-08-25 15:36:17 -0700

    update version number

--- doc/conf.py
+++ doc/conf.py
@@ -24,9 +24,9 @@ copyright = "2018-2021, Qiang Zhu, Scott Fredericks, Kevin Parrish"
 author = "Qiang Zhu, Scott Fredericks, Kevin Parrish"
 
 # The short X.Y version
-version = "0.3.4"
+version = "0.3.5"
 # The full version, including alpha/beta/rc tags
-release = "0.3.4"
+release = "0.3.5"
 
 
 # -- General configuration ---------------------------------------------------
