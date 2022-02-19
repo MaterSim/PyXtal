785168b2b66b41a4dd19b53419c82e93a07dcc06
Qiang Zhu <alecfans@gmail.com>
2022-02-19 14:05:37 -0800

    fix the lattice issue

--- pyxtal/__init__.py
+++ pyxtal/__init__.py
@@ -2229,7 +2229,10 @@ class pyxtal:
                 struc.atom_sites[j].update(coord)
             struc.source = 'Transition {:d} {:6.3f}'.format(i, max_disp*i)
             struc.disp = max_disp*i
-            struc.lattice.set_matrix(cell + i*l_disps)
+            if i >= N_images:
+                struc.lattice.set_matrix(cell + (i-2*(N_images-1))*l_disps)
+            else:
+                struc.lattice.set_matrix(cell + i*l_disps)
             strucs.append(struc)
         return strucs
 
