058f073d134724885db1420519e5c21cd4c34fe9
qzhu2017 <alecfans@gmail.com>
2022-05-16 07:56:31 -0700

    fix autotest

--- doc/conf.py
+++ doc/conf.py
@@ -24,9 +24,9 @@ copyright = "2018-2022, Qiang Zhu, Scott Fredericks, Kevin Parrish"
 author = "Qiang Zhu, Scott Fredericks, Kevin Parrish"
 
 # The short X.Y version
-version = "0.4.5"
+version = "0.4.6"
 # The full version, including alpha/beta/rc tags
-release = "0.4.5"
+release = "0.4.6"
 
 
 # -- General configuration ---------------------------------------------------
