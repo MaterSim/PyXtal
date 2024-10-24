19dbd8f6cb9ce64e4cb5a8746651572107e4e9b4
qzhu2017 <alecfans@gmail.com>
2024-10-24 13:39:40 -0400

    add mock modules

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,7 @@ from unittest.mock import MagicMock
 
 sys.path.insert(0, os.path.abspath("../"))
 # List of modules to mock
-MOCK_MODULES = ['torchani', 'lammps', 'pyshtools', 'mace']
+MOCK_MODULES = ['torchani', 'lammps', 'pyshtools', 'mace', 'mace.calculators']
 
 class Mock(MagicMock):
     @classmethod
