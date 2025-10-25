acc791506997a5b1868d1a5f58c696af67cde5c5
qzhu2017 <alecfans@gmail.com>
2025-10-25 15:19:10 -0400

    fix site symm bug for 227/228

--- tests/test_symmetry.py
+++ tests/test_symmetry.py
@@ -6,11 +6,9 @@ import unittest
 from pyxtal.lattice import Lattice
 from pyxtal.symmetry import Group, Wyckoff_position
 
-
 def resource_filename(package_name, resource_path):
     package_path = importlib.util.find_spec(package_name).submodule_search_locations[0]
     return os.path.join(package_path, resource_path)
-
 
 cif_path = resource_filename("pyxtal", "database/cifs/")
 l01 = Lattice.from_matrix([[4.08, 0, 0], [0, 9.13, 0], [0, 0, 5.50]])
@@ -22,23 +20,10 @@ wp2 = Wyckoff_position.from_group_and_letter(36, "4a")
 class TestSymmetry(unittest.TestCase):
     def test_from_symops_wo_grou(self):
         data = [
-            (["x, y, z", "-x, y+1/2, -z"], 4, 6),
-            (["x, y, z", "-x+1/2, -y, z+1/2", "-x, y, z", "x+1/2, -y, z+1/2"], 31, 155),
-            (
-                ["x, y, z", "-x, -y, -z", "-x+1/2, y+1/2, -z", "x+1/2, -y+1/2, z"],
-                14,
-                83,
-            ),
-            (
-                [
-                    "x, y, z",
-                    "-x, -y, -z",
-                    "-x+1/2, y+1/2, -z+1/2",
-                    "x+1/2, -y+1/2, z+1/2",
-                ],
-                14,
-                82,
-            ),
+        (["x, y, z", "-x, y+1/2, -z"], 4, 6),
+        (["x, y, z", "-x+1/2, -y, z+1/2", "-x, y, z", "x+1/2, -y, z+1/2"], 31, 155),
+        (["x, y, z", "-x, -y, -z", "-x+1/2, y+1/2, -z", "x+1/2, -y+1/2, z"], 14, 83),
+        (["x, y, z", "-x, -y, -z", "-x+1/2, y+1/2, -z+1/2", "x+1/2, -y+1/2, z+1/2"], 14, 82),
         ]
         for d in data:
             (strs, spg, hall) = d
@@ -48,8 +33,8 @@ class TestSymmetry(unittest.TestCase):
 
     def test_from_symops(self):
         data = [
-            (["x, y, z", "-x, y+1/2, -z"], 4, 6),
-            (["x, y, z", "-x+1/2, -y, z+1/2", "-x, y, z", "x+1/2, -y, z+1/2"], 31, 155),
+        (["x, y, z", "-x, y+1/2, -z"], 4, 6),
+        (["x, y, z", "-x+1/2, -y, z+1/2", "-x, y, z", "x+1/2, -y, z+1/2"], 31, 155),
         ]
         for d in data:
             (strs, spg, hall) = d
@@ -57,6 +42,41 @@ class TestSymmetry(unittest.TestCase):
             wp = Wyckoff_position.from_symops(strs, G)
             assert wp.number == spg
             assert wp.hall_number == hall
+
+
+    def test_reflections(self):
+        # 141: hkl: h+k+l=2n
+        # hk0: h,k=2n
+
+        data = [(230, 1, 1, 1, False),
+                (229, 1, 1, 1, False),
+                (228, 2, 1, 1, False),
+                (228, 2, 2, 0, True),
+                (227, 1, 1, 1, True),
+                (227, 2, 2, 0, True),
+                (226, 2, 2, 2, True),
+                (226, 2, 2, 0, True),
+                (225, 1, 1, 1, True),
+                (225, 2, 2, 1, False),
+                (142, 1, 1, 2, True),
+                (141, 1, 1, 2, True),
+                (141, 2, 2, 1, False),
+                (62, 1, 1, 4, True),
+                (62, 2, 2, 0, True),
+                (36, 1, 1, 8, True),
+                (36, 2, 2, 0, True),
+                (18, 3, 0, 0, False),
+                (17, 0, 0, 1, False),
+                (14, 3, 0, 0, True),
+                (14, 0, 0, 3, False),
+                (12, 1, 2, 0, False),
+                (2, 2, 2, 0, True),
+                ]
+        for d in data:
+            (spg, h, k, l, expected) = d
+            G = Group(spg)
+            print(spg, h, k, l, G.is_valid_hkl(h, k, l), expected)
+            assert G.is_valid_hkl(h, k, l) == expected
 
 if __name__ == "__main__":
     unittest.main()
